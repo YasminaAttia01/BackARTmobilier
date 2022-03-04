aea7b16dd8f9f7250dfa8b360f2ede5832b2a4ef
mombengue <doumomb2010@gmail.com>
2022-03-04 11:11:03 +0100

    Pages home and login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './layout/footer/footer.component';
-import { HeaderComponent } from './layout/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { AproposComponent } from './pages/apropos/apropos.component';
+import { CommandesComponent } from './pages/commandes/commandes.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ArticlesComponent,
+    AproposComponent,
+    CommandesComponent,
+    HeaderComponent,
     FooterComponent,
-    HeaderComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
