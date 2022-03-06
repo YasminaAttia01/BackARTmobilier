09b156100cadcc133be8189878dd84498c1bddf3
mombengue <doumomb2010@gmail.com>
2022-03-06 15:45:08 +0100

    Add article to cart in articleCoponent

--- frontend/src/app/component/products/products.component.ts
+++ frontend/src/app/component/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { IArticle } from 'src/app/models/article.model';
+import { CommandeService } from 'src/app/services/commande.service';
 import { ArticleService } from '../../services/article.service';
 
 @Component({
@@ -9,7 +11,9 @@ import { ArticleService } from '../../services/article.service';
 export class ProductsComponent implements OnInit {
   articles?: any;
 
-  constructor(private articleService: ArticleService) { }
+  constructor(
+    private articleService: ArticleService,
+    private commandeService: CommandeService) { }
 
   ngOnInit(): void {
     this.articleService.getAllArticles().subscribe(data => {
@@ -20,6 +24,10 @@ export class ProductsComponent implements OnInit {
         this.articles=data.message.articles
       }
     })
+  }
+
+  addToCart(article:IArticle){
+    this.commandeService.addItem(article);
   }
 
 }
